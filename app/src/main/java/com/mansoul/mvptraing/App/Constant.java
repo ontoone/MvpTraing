e3b8d4b6c3326fe8c8a4f8fd453436e6feb5e1a2
mansoul1994 <soulxiang@gmail.com>
2016-09-13 23:08:44 +0800

    test

--- app/src/main/java/com/mansoul/mvptraing/App/Constant.java
+++ app/src/main/java/com/mansoul/mvptraing/App/Constant.java
@@ -1,7 +1,7 @@
 package com.mansoul.mvptraing.app;
 
 /**
- * Created by Mansoul on 16/9/9.
+ * Created by Mansoul on 16/9/9.123
  */
 public class Constant {
     public static final String GANKGIRL_BASE_URL = "http://gank.io/api/data/";
